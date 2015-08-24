68563c03d7ce4ed3390a74ddd9c46a0d1248df85
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2015-08-24 17:01:21 -0400

    EventMap

--- Reflex.hs
+++ Reflex.hs
@@ -20,7 +20,7 @@ type Behavior a ≗ Time -> a
 -- number of Just values in the interval (s, t) is finite. I
 -- believe that somehting like this is technically necessary in
 -- the semantics of Push-Pull FRP as well.
---
+
 -- This representation of Events does however, restrict two Events from
 -- occuring at the same time. However, we can gain back the full generality by
 -- this using the merge function which, unlike the Push-Pull FRP semantics,
@@ -44,11 +44,28 @@ instance Functor Event where
 never :: Event a
 never ≗ λt -> Nothing
 
-pushE :: (a -> (b, Event b)) -> Event a -> Event (Behavior b)
-pushE f e ≗  λt -> (uncurry hold) (f $ e t) t
+type Push a ≗ Time -> a
 
-pushB :: (a -> Behavior (Maybe b)) -> Event a -> Event b
-pushB f e ≗ λt -> f (e t) t
+class EventMap m where
+  sample :: Behavior a -> Time -> a
+  hold   :: a -> Event a -> Time -> Behavior a
+
+instance EventMap Push where
+  sample ≗ id
+  hold a e t0 ≗ λt ->
+    let s ≗ [r | r > t0 && r < t && isJust (e r)]
+    -- Technically t shoud never be strictly less than t0;
+    -- this would signal an implementation error.
+    in if t <= t0 || null s
+         then a
+         -- Here we rely on the invariant that only a finte number
+         -- of Just values occur in the interval (t0, t) to insure
+         -- that the behavior changes after (not at the same time)
+         -- the event fires.
+         else fromJust (e (last s))
+
+push :: (a -> Push (Maybe b)) -> Event a -> Event b
+push f e ≗ \t -> e t >>= \a -> f a t
 
 merge :: Event a -> Event b -> Event (These a b)
 merge ea eb ≗ λt ->
@@ -65,22 +82,3 @@ switch b ≗ λt -> b t t
 -- hence we do not create Applicative or Monad instances.
 coincidence :: Event (Event a) -> Event a
 coincidence e ≗ λt -> e t >>= λf -> f t
-
-hold :: a -> Event a -> Time -> Behavior a
-hold a e t0 ≗ λt ->
-  let s ≗ [r | r > t0 && r < t && isJust (e r)]
-  -- Technically t shoud never be strictly less than t0;
-  -- this would signal an implementation error.
-  in if t <= t0 || null s
-       then a
-       -- Here we rely on the invariant that only a finte number
-       -- of Just values occur in the interval (t0, t) to insure
-       -- that the behavior changes after (not at the same time)
-       -- the event fires.
-       else fromJust (e (last s))
-
--- XXX Do you think we should include this?
--- Not really part of the denotational semantics as it is defined in
--- terms of hold.
-switcher :: Behavior a -> Event (Behavior a) -> Time -> Behavior a
-swithcer b eb t0 = λt -> hold b eb t0 t t
