82a2a3b5f925cb31ca3629d216fb2b1d3c70db07
Jeffrey Rosenbluth <jeffrey.rosenbluth@gmail.com>
2015-08-25 10:10:00 -0400

    clean up and comments

--- Reflex.hs
+++ Reflex.hs
@@ -1,34 +1,38 @@
-------------------------------------------------------------------
--- Reflex FRP
-------------------------------------------------------------------
--- The denotational semantics of Reflex FRP
--- We take (≗) to indicate semantic equality.
--- In other words, the left hand side of an equals sign is haskell
--- syntax and the right hand side is its mathematical meaning.
-------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- |
+-- The interface and denotational semantics of Reflex FRP.
+-- (c) 2015 Ryan Trinkle & Jeffrey Rosenbluth.
+--
+-- We take (≗) to indicate semantic equality and interpret instances
+-- as semantic instances.
+--------------------------------------------------------------------------------
 
--- Not part of the Reflex interface ------------------------------
+-- | Not part of the Reflex interface ------------------------------------------
 data These a b ≗ This a | That b | These a b
 
--- Any totally ordered set.
-type Time  ≗  (Eq a, Ord a) => a
-------------------------------------------------------------------
+-- | Any totally ordered set.
+type Time ≗ (Eq a, Ord a) => a
+--------------------------------------------------------------------------------
 
+-- | Behaviors can be sampled at will, but it is not possible to be notified
+--   when they change.
 type Behavior a ≗ Time -> a
 
--- Events must satifsy the constraint that for any two Times s < t, the
--- number of Just values in the interval (s, t) is finite. I
--- believe that somehting like this is technically necessary in
--- the semantics of Push-Pull FRP as well.
-
--- This representation of Events does however, restrict two Events from
--- occuring at the same time. However, we can gain back the full generality by
--- this using the merge function which, unlike the Push-Pull FRP semantics,
--- keeps These values of simultaneous events.
+-- | Events must satifsy the constraint that for any two Times s < t, the
+--   number of Just values in the interval (s, t) is finite. (I
+--   believe that somehting like this is technically necessary in
+--   the semantics of Push-Pull FRP as well).
+--   This representation of Events does however, restrict multiple Events from
+--   occuring at the same time. However, we can gain back the full generality by
+--   using the 'merge; function, which in contract to the Push-Pull FRP semantics,
+--   keeps both values of simultaneous events.
 type Event a ≗ Time -> Maybe a
 
+-- | The Push type is used to limit the mapping function passed to 'push'. It
+--   may only 'sample' Behaviors and 'hold' Events.
 type Push a ≗ Time -> a
 
+-- | Semantics Instances.
 instance Functor Behavior where
   fmap f b ≗ λt -> f . b $ t
 
@@ -43,28 +47,23 @@ instance Monad Behavior where
 instance Functor Event where
   fmap f e ≗ λt -> f <$> e t
 
+instance Functor Push where
+  fmap f p ≗ λt -> f . p $ t
+
+instance Applicative Push where
+  pure a  ≗ const a
+  f <*> x ≗ λt -> f t (x t)
+
+instance Monad Push where
+  return ≗ pure
+  f >>= k ≗ λt -> k (f t) t
+
+-- | An Event with no occurrences.
 never :: Event a
 never ≗ λt -> Nothing
 
-sample :: Behavior a -> Push a
-sample ≗ id
-
-hold   :: a -> Event a -> Push (Behavior a)
-hold a e t0 ≗ λt ->
-  let s ≗ [r | r > t0 && r < t && isJust (e r)]
-  -- Technically t shoud never be strictly less than t0;
-  -- this would signal an implementation error.
-  in if t <= t0 || null s
-       then a
-       -- Here we rely on the fact that only a finte number
-       -- of Just values occur in the interval (t0, t) to insure
-       -- that the behavior changes after (not at the same time)
-       -- the event fires.
-       else fromJust (e (last s))
-
-push :: (a -> Push (Maybe b)) -> Event a -> Event b
-push f e ≗ \t -> e t >>= \a -> f a t
-
+-- | Merge two events; the resulting Event will only occur if at least one input
+--   event is occuring.
 merge :: Event a -> Event b -> Event (These a b)
 merge ea eb ≗ λt ->
   case (ea t, eb t) of
@@ -73,10 +72,37 @@ merge ea eb ≗ λt ->
     (Nothing, Just b ) -> Just (That b)
     (Just a , Just b ) -> Just (These a b)
 
+-- | Get the current value in the Behavior.
+sample :: Behavior a -> Push a
+sample ≗ id
+
+-- | Create a new Behavior whose value will initially be equal to the given
+--   value and will be updated whenever the given Event occurs.
+--   Technically t shoud never be strictly less than t0; this would signal an
+--   implementation error. We rely on the fact that only a finte number of
+--   Just values occur in the interval (t0, t) to insure that the behavior
+--   changes after (not at the same time) the event fires.
+hold   :: a -> Event a -> Push (Behavior a)
+hold a e t0 ≗ λt ->
+  let s ≗ [r | r > t0 && r < t && isJust (e r)]
+  in if t <= t0 || null s
+       then a
+       else fromJust (e (last s))
+
+-- | Create an Event from another Event; the provided function can 'sample'
+--   Behaviors and 'hold' Events, and use the results to produce an occurring
+--   (Just) or non-occurring (Nothing) result.
+push :: (a -> Push (Maybe b)) -> Event a -> Event b
+push f e ≗ \t -> e t >>= \a -> f a t
+
+-- | Create an Event that will occur whenever the currently-selected input
+--   Event occurs.
 switch :: Behavior (Event a) -> Event a
 switch b ≗ λt -> b t t
 
--- There is no useful definition of Pure for Events,
--- hence we do not create Applicative or Monad instances.
+-- | Create an Event that will occur whenever the input event is occurring and
+--   its occurrence value, another Event, is also occurring.
+--   Since there is no useful definition of Pure for Events,
+--   we do not create applicative and monad instances.
 coincidence :: Event (Event a) -> Event a
 coincidence e ≗ λt -> e t >>= λf -> f t
